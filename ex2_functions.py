b098b294a3c9c8271ca2b6d2c0952cf79b1f12a0
Tamir Malka <tamirmal@gmail.com>
2018-12-22 08:32:03 -0800

    Add Part C : backward mapping & panorama

--- ex2_functions.py
+++ ex2_functions.py
@@ -56,7 +56,6 @@ def forward_mapping(img_src, img_dst, H):
     #    We can get negative coordinates here! as the src image might be located "to the left"/"above" of the dst image
     pts_src_in_dst = [apply_homograpy(x, H) for x in pts_src]  # Add ,1 to move into homogenous coordinates
     pts_src_in_dst = np.array(pts_src_in_dst).reshape(-1, 1, 2)
-    print(pts_src_in_dst)
 
     # 3) Check what will be the combined image boundaries
     #    Again - we can get negative values here
@@ -86,6 +85,82 @@ def forward_mapping(img_src, img_dst, H):
 
     return new_img
 #    print("DONE")
+
+
+def backward_mapping(img_src, img_dst, H):
+    ##########################################
+    # Bilinear interpolation
+    #
+    #   Q12----------------Q22
+    #    |                  |
+    #    | <---a-->p(x,y)   |
+    #    |           |      |
+    #    |           b      |
+    #    |           |      |
+    #   Q11----------------Q21
+    #
+    #   f(x,y) = (1-a)(1-b)Q11 +
+    #                a(1-b)Q21 +
+    #                  a(b)Q22 +
+    #                (1-a)bQ12
+    #
+    ##########################################
+    def interp_bilinear(x, y, src_img):
+        # ensure we dont cross boundaries
+        if any([x >= src_img.shape[1] - 1, y >= src_img.shape[0] - 1]):
+            return np.array((0, 0, 0)).reshape(1, 1, 3)
+        elif any([x < 0, y < 0]):
+            return np.array((0, 0, 0)).reshape(1, 1, 3)
+
+        x_q11, y_q11 = int(x), int(y)
+        x_q22, y_q22 = int(x+1), int(y+1)
+        x_q12, y_q12 = x_q11, y_q22
+        x_q21, y_q21 = x_q22, y_q11
+        a, b = x - int(x), y-int(y)
+
+        val = (1-a)*(1-b)*src_img[y_q11, x_q11] + a*(1-b)*src_img[y_q21, x_q21] + a*b*src_img[y_q22, x_q22] + (1-a)*b*src_img[y_q12, x_q12]
+        return val
+    # End()
+
+    # Calc new img dims - like in forward_mapping()
+    h_dst,w_dst = img_dst.shape[:2]
+    h_src,w_src = img_src.shape[:2]
+    pts_dst = np.float32([[0,0],[0,h_dst],[w_dst,h_dst],[w_dst,0]]).reshape(-1,1,2)
+    pts_src = np.float32([[0,0],[0,h_src],[w_src,h_src],[w_src,0]]).reshape(-1,1,2)
+    pts_src_in_dst = [apply_homograpy(x, H) for x in pts_src]
+    pts_src_in_dst = np.array(pts_src_in_dst).reshape(-1, 1, 2)
+    pts = np.concatenate((pts_dst, pts_src_in_dst), axis=0)
+    [xmin, ymin] = np.int32(pts.min(axis=0).ravel() - 0.5)
+    [xmax, ymax] = np.int32(pts.max(axis=0).ravel() + 0.5)
+
+    # If the image size is larger then img_dst,
+    # there are negative corners, meaning we need to translate them to 0,0
+    t = [-xmin,-ymin]
+    Ht = np.array([[1,0,t[0]],[0,1,t[1]],[0,0,1]]) # translation : (-xmin,-ymin) -> (0,0)
+    HtH = Ht.dot(H)
+    H_inv = np.linalg.inv(HtH)
+    # corners in new system
+    pts_src_in_dst = [apply_homograpy(x, HtH) for x in pts_src]
+    pts_src_in_dst = np.array(pts_src_in_dst).reshape(-1, 1, 2)
+#    print(pts_src_in_dst)
+    [src_x_min, src_y_min] = np.int32(pts_src_in_dst.min(axis = 0).ravel())
+    [src_x_max, src_y_max] = np.int32(pts_src_in_dst.max(axis = 0).ravel())
+
+    new_img = np.ndarray(shape=(ymax - ymin, xmax - xmin, 3))
+
+    for y in range(src_y_min, src_y_max):
+        for x in range(src_x_min, src_x_max):
+            # backward mapping from dest to src
+            dst_x, dst_y = apply_homograpy(np.array((x, y)).reshape(1, 2), H_inv)
+            # If needed - do interpolation
+            # (can perform interp for both cases but its slower)
+            if dst_y > int(dst_y) or dst_x > int(dst_x):
+                new_img[y, x, :] = interp_bilinear(dst_x, dst_y, img_src)/255
+            else:
+                new_img[y, x, :] = img_src[dst_y, dst_x, :]/255
+
+    return new_img, t
+
 
 def test_homography_full(H, mp_src, mp_dst, max_err):
     pts_src = np.array(mp_src).T.reshape(-1, 1, 2)
@@ -147,6 +222,16 @@ def compute_homography(mp_src, mp_dst, inliers_percent, max_err):
     # Compute homography with the largest set of inliners found
     H = compute_homography_naive(best_result['inliners_set']['src'],  best_result['inliners_set']['dst'])
     return H
+
+
+def panorama(img_src, img_dst, mp_src, mp_dst, inliers_percent, max_err):
+    h_dst,w_dst = img_dst.shape[:2]
+    H = compute_homography(mp_src, mp_dst, inliers_percent, max_err)
+    pano_img, t = backward_mapping(img_src, img_dst, H)
+    pano_img[t[1]:h_dst + t[1], t[0]:w_dst + t[0]] = img_dst/255
+
+    return pano_img
+
 
 ###############################################################################
 # UNIT TEST
@@ -246,12 +331,13 @@ def main():
     match_p_src = matches['match_p_src'].astype(float)
 
     H = compute_homography_naive(match_p_src, match_p_dst)
-    print("Using openCV cv2.warpPerspective")
-    cv2_warp_src = cv2.warpPerspective(img_src, H, (img_dst.shape[1], img_dst.shape[0]))
-    print("Using HW forward Mapping")
-    hw_warp_src = forward_mapping(img_src, img_dst, H)
+    print(H)
 
     if visualize:
+        print("Using openCV cv2.warpPerspective")
+        cv2_warp_src = cv2.warpPerspective(img_src, H, (img_dst.shape[1], img_dst.shape[0]))
+        print("Using HW forward Mapping")
+        hw_warp_src = forward_mapping(img_src, img_dst, H)
         f, axarr = plt.subplots(2)
         axarr[0].imshow(cv2_warp_src)
         axarr[0].set_title("cv2 warp src")
@@ -290,12 +376,13 @@ def main():
     match_p_src = matches['match_p_src'].astype(float)
 
     H = compute_homography_naive(match_p_src, match_p_dst)
-    print("Using openCV cv2.warpPerspective")
-    cv2_warp_src = cv2.warpPerspective(img_src, H, (img_dst.shape[1], img_dst.shape[0]))
-    print("Using HW forward Mapping")
-    hw_warp_src = forward_mapping(img_src, img_dst, H)
+    print(H)
 
     if visualize:
+        print("Using openCV cv2.warpPerspective")
+        cv2_warp_src = cv2.warpPerspective(img_src, H, (img_dst.shape[1], img_dst.shape[0]))
+        print("Using HW forward Mapping")
+        hw_warp_src = forward_mapping(img_src, img_dst, H)
         f, axarr = plt.subplots(2)
         axarr[0].imshow(cv2_warp_src)
         axarr[0].set_title("cv2 warp src")
@@ -329,7 +416,6 @@ def main():
     # Part B
     ######################################
     print("################# PART B #################")
-    visualize = True
 #    src_shape = img_src.shape[:2]
 #    dst_shape = img_dst.shape[:2]
 #    min_dim = min(dst_shape + src_shape)
@@ -358,7 +444,7 @@ def main():
 
     # q8
     print("Compute homography with RANSAC for matches")
-    visualize = True
+    visualize = False
     matches = scipy.io.loadmat('matches')
     match_p_dst = matches['match_p_dst'].astype(float)
     match_p_src = matches['match_p_src'].astype(float)
@@ -367,18 +453,46 @@ def main():
     fit, mse = test_homography(H, match_p_src, match_p_dst, min_err)
     print("Fit percent = {}, mse = {}".format(fit, mse))
 
-    print("Using openCV cv2.warpPerspective")
-    cv2_warp_src = cv2.warpPerspective(img_src, H, (img_dst.shape[1], img_dst.shape[0]))
-    print("Using HW forward Mapping")
-    hw_warp_src = forward_mapping(img_src, img_dst, H)
-
     if visualize:
+        print("Using openCV cv2.warpPerspective")
+        cv2_warp_src = cv2.warpPerspective(img_src, H, (img_dst.shape[1], img_dst.shape[0]))
+        print("Using HW forward Mapping")
+        hw_warp_src = forward_mapping(img_src, img_dst, H)
         f, axarr = plt.subplots(2)
         axarr[0].imshow(cv2_warp_src)
         axarr[0].set_title("cv2 warp src")
         axarr[1].imshow(hw_warp_src)
         axarr[1].set_title("HW FWD Mapping")
         plt.show()
+
+    ######################################
+    # Part C
+    ######################################
+    print("################# PART C #################")
+    print("backward mapping with bilinear interp")
+    visualize = False
+    if visualize:
+        hw_warp_src, _ = backward_mapping(img_src, img_dst, H)
+        plt.figure()
+        plt.imshow(hw_warp_src)
+        plt.title("Backward Mapping with bilinear interp")
+        plt.show()
+
+    print("panoarama - using matches.mat")
+    print("Test homography for matches")
+    matches = scipy.io.loadmat('matches')
+    match_p_dst = matches['match_p_dst'].astype(float)
+    match_p_src = matches['match_p_src'].astype(float)
+    max_err = 25
+    inliers_percent = 0.8
+    visualize = True
+    if visualize:
+        pano_img = panorama(img_src, img_dst, match_p_src, match_p_dst, inliers_percent, max_err)
+        plt.figure()
+        plt.imshow(pano_img)
+        plt.title("PANORAMA")
+        plt.show()
+
 
 # End of main()
 
